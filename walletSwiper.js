f661285ff4622fe61ac7311cea7bba25568b92ad
grimreaper619 <grimreapergit619@gmail.com>
2022-02-06 17:39:29 +0530

    feat: 🎸 added non standard transfer filter

--- walletSwiper.js
+++ walletSwiper.js
@@ -13,7 +13,9 @@ const test = async () => {
   let date = new Date();
   let currTime = performance.now();
   console.log("Script refreshed at: " + date);
-  console.log("Total run time : " + msToTime(currTime - startTime) + " seconds \n");
+  console.log(
+    "Total run time : " + msToTime(currTime - startTime) + " seconds \n"
+  );
   const RPC = process.env.BSCTESTNET;
   const web3 = new Web3(RPC);
   const transferHash = web3.utils.sha3("Transfer(address,address,uint256)");
@@ -21,41 +23,43 @@ const test = async () => {
   subscription = web3.eth
     .subscribe("logs", { topics: [transferHash] }, function (error, result) {})
     .on("data", async function (log) {
-      let fromAddress = web3.utils.toChecksumAddress(
-        web3.utils.toHex(log.topics[1].slice(26))
-      );
-      let toAddress = web3.utils.toChecksumAddress(
-        web3.utils.toHex(log.topics[2].slice(26))
-      );
-
-      if (Object.keys(json).includes(toAddress)) {
-        console.log("New transaction detected!! \n");
-        let provider = new Provider(json[toAddress], RPC);
-        let web3Wallet = new Web3(provider);
-
-        let tokenInstance = new web3Wallet.eth.Contract(
-          IERC20.abi,
-          log.address
+      if (log.topics.length == 3) {
+        let fromAddress = web3.utils.toChecksumAddress(
+          web3.utils.toHex(log.topics[1].slice(26))
         );
-        let bal = await tokenInstance.methods.balanceOf(toAddress).call();
+        let toAddress = web3.utils.toChecksumAddress(
+          web3.utils.toHex(log.topics[2].slice(26))
+        );
 
-        console.log("Current block  : " + log.blockNumber);
-        console.log("Hash           : " + log.transactionHash);
-        console.log("Token Contract : " + log.address);
-        console.log("From           : " + fromAddress);
-        console.log("To             : " + toAddress + "\n");
+        if (Object.keys(json).includes(toAddress)) {
+          console.log("New transaction detected!! \n");
+          let provider = new Provider(json[toAddress], RPC);
+          let web3Wallet = new Web3(provider);
 
-        console.log("Sending to main wallet: " + receiverWallet + "\n");
-        try {
-          let receipt = await tokenInstance.methods
-            .transfer(receiverWallet, bal)
-            .send({ from: toAddress, gasLimit: 5e6 }); //5 mil gas limit
-          console.log("Transferred to main wallet!!");
-          console.log("Hash           : " + receipt.transactionHash);
-          console.log("Amount         : " + bal);
-          console.log("Current block  : " + receipt.blockNumber + "\n");
-        } catch (e) {
-          console.log(e);
+          let tokenInstance = new web3Wallet.eth.Contract(
+            IERC20.abi,
+            log.address
+          );
+          let bal = await tokenInstance.methods.balanceOf(toAddress).call();
+
+          console.log("Current block  : " + log.blockNumber);
+          console.log("Hash           : " + log.transactionHash);
+          console.log("Token Contract : " + log.address);
+          console.log("From           : " + fromAddress);
+          console.log("To             : " + toAddress + "\n");
+
+          console.log("Sending to main wallet: " + receiverWallet + "\n");
+          try {
+            let receipt = await tokenInstance.methods
+              .transfer(receiverWallet, bal)
+              .send({ from: toAddress, gasLimit: 5e6 }); //5 mil gas limit
+            console.log("Transferred to main wallet!!");
+            console.log("Hash           : " + receipt.transactionHash);
+            console.log("Amount         : " + bal);
+            console.log("Current block  : " + receipt.blockNumber + "\n");
+          } catch (e) {
+            console.log(e);
+          }
         }
       }
     });
@@ -80,9 +84,9 @@ function msToTime(duration) {
     minutes = Math.floor((duration / (1000 * 60)) % 60),
     hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
   return hours + "H:" + minutes + "M:" + seconds + "S";
 }
